9ee6b9ec97cec4bacbbb40ea5c2a0f23cc2befe4
uiratan <uiratan@gmail.com>
2023-05-26 13:08:03 -0300

    Enrollment, EnrollmentPK

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
@@ -0,0 +1,92 @@
+package com.devsuperior.dslearnbds.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
+
+@Entity
+@Table(name = "tb_enrollment")
+public class Enrollment implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private EnrollmentPK id = new EnrollmentPK();
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant enrollMoment;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant refundMoment;
+	
+	private boolean avaiable;
+	private boolean onlyUpdate;
+	
+	public Enrollment() {}
+
+	public Enrollment(User user, Offer offer, Instant enrollMoment, Instant refundMoment, boolean avaiable,
+			boolean onlyUpdate) {
+		super();
+		this.id.setUser(user);
+		this.id.setOffer(offer);
+		this.enrollMoment = enrollMoment;
+		this.refundMoment = refundMoment;
+		this.avaiable = avaiable;
+		this.onlyUpdate = onlyUpdate;
+	}
+
+	public User getStudent() {
+		return this.id.getUser();
+	}
+
+	public void setStudent(User user) {
+		this.id.setUser(user);
+	}
+	
+	public Offer getOffer() {
+		return this.id.getOffer();
+	}
+	
+	public void setOffer(Offer offer) {
+		this.id.setOffer(offer);
+	}
+	
+	public Instant getEnrollMoment() {
+		return enrollMoment;
+	}
+
+	public void setEnrollMoment(Instant enrollMoment) {
+		this.enrollMoment = enrollMoment;
+	}
+
+	public Instant getRefundMoment() {
+		return refundMoment;
+	}
+
+	public void setRefundMoment(Instant refundMoment) {
+		this.refundMoment = refundMoment;
+	}
+
+	public boolean isAvaiable() {
+		return avaiable;
+	}
+
+	public void setAvaiable(boolean avaiable) {
+		this.avaiable = avaiable;
+	}
+
+	public boolean isOnlyUpdate() {
+		return onlyUpdate;
+	}
+
+	public void setOnlyUpdate(boolean onlyUpdate) {
+		this.onlyUpdate = onlyUpdate;
+	}
+	
+	
+}
