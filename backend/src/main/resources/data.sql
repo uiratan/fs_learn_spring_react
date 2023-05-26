3a715fc9430899298ba3b7f3477a080f5d4f0c5b
uiratan <uiratan@gmail.com>
2023-05-25 21:19:39 -0300

    Entities Course, Offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,8 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://cdn.pixabay.com/photo/2018/03/22/10/55/training-course-3250007_1280.jpg', 'https://thumbs.dreamstime.com/z/studying-book-icon-146773785.jpg');
+
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2023-07-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2024-07-13T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2023-09-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2024-09-13T03:00:00Z', 1);
