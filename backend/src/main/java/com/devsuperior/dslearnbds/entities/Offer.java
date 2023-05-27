b5c9195673e0b113509c6935990ca2624741459f
uiratan <uiratan@gmail.com>
2023-05-27 11:42:06 -0300

    Topic

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
@@ -38,6 +38,11 @@ public class Offer implements Serializable {
 	@OneToMany(mappedBy = "offer")
 	private List<Resource> resources = new ArrayList<>();
 	
+	@OneToMany(mappedBy = "offer")
+	private List<Topic> topics = new ArrayList<>();
+	
+
+
 	public Offer() {
 	}
 
@@ -94,6 +99,10 @@ public class Offer implements Serializable {
 		return resources;
 	}
 
+	public List<Topic> getTopics() {
+		return topics;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
